29979981b6a72456d829fa268593428e7b6bda59
chibwesamuel <smchibwe@yahoo.com>
2024-01-29 11:30:23 +0200

    Initial tasks submission

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints a message to the standard output (STDOUT).
+ * @param {string} message - The message to be printed.
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
+
