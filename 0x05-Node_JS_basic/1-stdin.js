633adeb56f3f5d3d5383c59b0eb1f2755b5f8b10
chibwesamuel <smchibwe@yahoo.com>
2024-01-31 13:25:50 +0200

    Fixed 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
 // Display the initial message
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 // Read input from the user
 process.stdin.once('data', (input) => {
-    // Extract and trim the input
-    const name = input.toString().trim();
+  // Extract and trim the input
+  const name = input.toString().trim();
 
-    // Display the user's name
-    console.log(`Your name is: ${name}`);
+  // Display the user's name
+  console.log(`Your name is: ${name}`);
 
-    // Display closing message
-    console.log("This important software is now closing");
+  // Display closing message
+  console.log('This important software is now closing');
 });
