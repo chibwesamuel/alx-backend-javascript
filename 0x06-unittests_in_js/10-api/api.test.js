ea29dedae05ca83a7f6937c00b68becfe930a704
chibwesamuel <smchibwe@yahoo.com>
2024-02-01 21:19:59 +0200

    All tasks done

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,28 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Login endpoint', () => {
+  it('should return welcome message with username', (done) => {
+    const options = {
+      url: 'http://localhost:7865/login',
+      method: 'POST',
+      json: { userName: 'Betty' }
+    };
+
+    request(options, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome Betty');
+      done();
+    });
+  });
+});
+
+describe('Available Payments endpoint', () => {
+  it('should return available payment methods', (done) => {
+    request('http://localhost:7865/available_payments', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+      done();
+    });
+  });
+});
