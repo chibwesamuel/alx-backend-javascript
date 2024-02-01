e1c2f89f887f32587acc2f2bfffd808269606f87
chibwesamuel <smchibwe@yahoo.com>
2024-02-01 21:12:21 +0200

    9-api take 1

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Cart page', () => {
+  it('Correct status code when :id is a number', (done) => {
+    request('http://localhost:7865/cart/123', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct status code when :id is NOT a number (=> 404)', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
