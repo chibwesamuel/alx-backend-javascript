0ed50c58dca3536720f82b50a1b46adca6f01ac6
chibwesamuel <smchibwe@yahoo.com>
2024-02-01 21:01:52 +0200

    8-api directory and files created

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,25 @@
+// api.test.js
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  it('Correct status code?', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Other?', (done) => {
+    // Add other tests as needed
+    done();
+  });
+});
+
