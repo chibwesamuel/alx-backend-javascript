04036abea243c8535ba9befc34de57bb831b62fb
chibwesamuel <smchibwe@yahoo.com>
2024-02-03 22:27:24 +0200

    New updates to code

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,16 +1,38 @@
+const { describe, it } = require('mocha');
 const request = require('request');
 const { expect } = require('chai');
 
 describe('Cart page', () => {
+  it('Test for GET / exists', (done) => {
+    request('http://localhost:7865/', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Test for GET / returns “Welcome to the payment system” exists', (done) => {
+    request('http://localhost:7865/', (error, response) => {
+      expect(response.body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Test for GET /cart/:id returns “Payment methods for cart :id”', (done) => {
+    request('http://localhost:7865/cart/123', (error, response) => {
+      expect(response.body).to.equal('Payment methods for cart 123');
+      done();
+    });
+  });
+
   it('Correct status code when :id is a number', (done) => {
-    request('http://localhost:7865/cart/123', (error, response, body) => {
+    request('http://localhost:7865/cart/123', (error, response) => {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
   it('Correct status code when :id is NOT a number (=> 404)', (done) => {
-    request('http://localhost:7865/cart/hello', (error, response, body) => {
+    request('http://localhost:7865/cart/hello', (error, response) => {
       expect(response.statusCode).to.equal(404);
       done();
     });
