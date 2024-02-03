ae2d18cdf1bb1bdc77de8bd9f1506fa9ae9d2a67
chibwesamuel <smchibwe@yahoo.com>
2024-02-03 22:51:06 +0200

    Task 10 revisit

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,3 +1,4 @@
+const { describe, it } = require('mocha');
 const request = require('request');
 const { expect } = require('chai');
 
@@ -9,9 +10,9 @@ describe('Login endpoint', () => {
       json: { userName: 'Betty' }
     };
 
-    request(options, (error, response, body) => {
+    request(options, (error, response) => {
       expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome Betty');
+      expect(response.body).to.equal('Welcome Betty');
       done();
     });
   });
@@ -19,9 +20,46 @@ describe('Login endpoint', () => {
 
 describe('Available Payments endpoint', () => {
   it('should return available payment methods', (done) => {
-    request('http://localhost:7865/available_payments', (error, response, body) => {
+    request('http://localhost:7865/available_payments', (error, response) => {
       expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+      expect(response.body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+      done();
+    });
+  });
+});
+
+describe('Cart endpoint', () => {
+  it('Test for GET / exists', (done) => {
+    request('http://localhost:7865/cart', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Test for GET / returns “Welcome to the payment system” exists', (done) => {
+    request('http://localhost:7865/cart', (error, response) => {
+      expect(response.body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('Test for GET /cart/:id exists', (done) => {
+    request('http://localhost:7865/cart/123', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Test for GET /cart/:id returns “Payment methods for cart :id”', (done) => {
+    request('http://localhost:7865/cart/123', (error, response) => {
+      expect(response.body).to.equal('Payment methods for cart 123');
+      done();
+    });
+  });
+
+  it('Test for GET /cart/:id can’t accept :id not a number', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response) => {
+      expect(response.statusCode).to.equal(404);
       done();
     });
   });
