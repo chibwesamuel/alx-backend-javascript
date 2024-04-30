2712f1b83e21a2a06c1da67d8e6cda40193b9243
chibwesamuel <smchibwe@yahoo.com>
2024-04-30 17:24:07 +0200

    Worked on child process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,37 +1,14 @@
-// Function to handle input
-const handleInput = (input) => {
+// Display the initial message
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// Read input from the user
+process.stdin.once('data', (input) => {
   // Extract and trim the input
   const name = input.toString().trim();
 
   // Display the user's name
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\n`);
 
   // Display closing message
-  console.log('This important software is now closing');
-
-  // Exit the process
-  process.exit();
-};
-
-// Display the initial message
-console.log('Welcome to Holberton School, what is your name?');
-
-// Check if there is data in the pipeline
-if (process.stdin.isTTY) {
-  // Read input from the user
-  process.stdin.once('data', (input) => {
-    handleInput(input);
-  });
-} else {
-  // Read input from the pipeline
-  let data = '';
-  process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk !== null) {
-      data += chunk;
-    }
-  });
-  process.stdin.on('end', () => {
-    handleInput(data);
-  });
-}
+  process.stdout.write('This important software is now closing\n');
+});
