b19afcaf4f9d5f3adc714e381126cb0f31008246
chibwesamuel <smchibwe@yahoo.com>
2024-04-30 16:14:31 +0200

    Initialized project

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,9 +0,0 @@
-/**
- * Prints a given message to the standard output (STDOUT).
- * @param {string} message - The message to be printed.
- */
-const displayMessage = (message) => {
-  console.log(message);
-};
-
-module.exports = displayMessage;
