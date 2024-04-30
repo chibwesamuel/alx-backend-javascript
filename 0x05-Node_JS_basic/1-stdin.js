d9d036db9e607c9e0d19df9855a25f33f381ef99
chibwesamuel <smchibwe@yahoo.com>
2024-04-30 17:13:30 +0200

    1-stdin fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,28 @@
 // Display the initial message
 console.log('Welcome to Holberton School, what is your name?');
 
-// Read input from the user
-process.stdin.once('data', (input) => {
+// Check if there is data in the pipeline
+if (process.stdin.isTTY) {
+  // Read input from the user
+  process.stdin.once('data', (input) => {
+    handleInput(input);
+  });
+} else {
+  // Read input from the pipeline
+  let data = '';
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    if (chunk !== null) {
+      data += chunk;
+    }
+  });
+  process.stdin.on('end', () => {
+    handleInput(data);
+  });
+}
+
+// Function to handle input
+const handleInput = (input) => {
   // Extract and trim the input
   const name = input.toString().trim();
 
@@ -14,4 +34,4 @@ process.stdin.once('data', (input) => {
 
   // Exit the process
   process.exit();
-});
+};
