5d8f42442ff38739daf6aea4e5d631e68a2ae18c
chibwesamuel <smchibwe@yahoo.com>
2024-04-30 16:34:32 +0200

    Added task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Will print a given message to STDOUT.
+ * @param {string} message - The message to be printed.
+ */
+const displayMessage = (message) => {
+    console.log(message);
+};
+
+module.exports = displayMessage;
