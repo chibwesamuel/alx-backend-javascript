d4328e1dfb87f3525967978b787e0e926a488ebe
chibwesamuel <smchibwe@yahoo.com>
2024-05-04 08:55:46 +0200

    http fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,8 +5,15 @@ const app = http.createServer((req, res) => {
   // Set the response headers
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  // Send the response body
-  res.end('Hello Holberton School!\n');
+  // Check the request URL
+  if (req.url === '/' || req.url === '/test') {
+    // Send the response body
+    res.end('Hello Holberton School!\n');
+  } else {
+    // Send a 404 response for other endpoints
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found\n');
+  }
 });
 
 // Listen on port 1245
